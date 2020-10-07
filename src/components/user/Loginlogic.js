fa47b8f9335e87df198925357fe61933bc9f2153
kmin-jeong <desk535353@gmail.com>
2020-10-08 02:33:26 +0900

    id,password 빈칸으로 폼 제출되지 못하게 하기 submit은 폼을 넘겨야 하니까 const로 지정&id,password는 setID,setPassword로 지정

--- src/components/user/Loginlogic.js
+++ src/components/user/Loginlogic.js
@@ -1,21 +1,61 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+// 로그인에 들어가는 id,password
+export default () => {
 
-this.state = {
-    id = "",
-    password = ""
-}
+    const [Id, setId] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(setFooterVisible(false))
+        dispatch(setHeaderVisible(false))
+        return () => {
+            dispatch(setHeaderVisible(true))
+        dispatch(setFooterVisible(true))
+        }
+     })
+
 idhandle = (e) =>{
-    this.setstate({
-        id:e.target.value
-    });
+    setId(e.currentTarget.value);
 }
 passwordhandle = (e) =>{
-    this.setstate({
-        password:e.target.value
-    });
+   setPassword(e.currentTarget.value);
 }
-submithandle = (e) => {
-    e.preventDefault();
-​}
+
+const submit = (Login) => {
+    return (
+        // 사이트 주소 비워둠
+         fetch("", Form)
+           .then(alert("1"))
+          .then(this.props.history.push("/"))
+          .catch((err) => {
+            throw alert("");
+          })
+      );
+}
+
+
+
+  return(
+      <Login>
+        <G1><img src={g1}/></G1>
+        <G2><img src={g2}/></G2>
+        <G3><img src={g3}/></G3>
+        <Title>Login</Title>
+        <LoginForm>
+          <label>아이디</label>
+          <input type="text" name="id"/>
+          <label>비밀번호</label>
+          <input type="password" name="password"/>
+          <Forgot>비밀번호를 잊으셨나요?</Forgot>
+          <LoginButton>로그인</LoginButton>
+          <Join>회원이 아니십니까?<Link to='/join'>회원 가입</Link></Join>
+          <SocialLogin>
+            <p>다음 계정으로 로그인</p>
+            <img src={kakao}/>
+          </SocialLogin>
+        </LoginForm>
+      </Login>
+    );
+}
 
