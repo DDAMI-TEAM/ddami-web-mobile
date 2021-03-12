90d5d042e44a96610b355756a7118ac711ccf56a
sbyeol3 <ahnsbyeol3@gmail.com>
2021-03-12 23:42:00 +0900

    Style : prettier

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,19 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-import {Link} from "react-router-dom";
-import {useSelector} from "react-redux";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Footer = styled.div`
-  background-color: #F1EFFE;
+  background-color: #f1effe;
   padding: 38px 63px;
   font-size: 13px;
   text-align: center;
   & > a {
-    color: #3C3C3C;
+    color: #3c3c3c;
     text-decoration: none;
     margin: 0 8px;
   }
-`
+`;
 
 const Copyright = styled.div`
   font-size: 12px;
@@ -21,16 +21,21 @@ const Copyright = styled.div`
 `;
 
 export default () => {
-  const { state } = useSelector(store => {return store.footer});
+  const { state } = useSelector((store) => {
+    return store.footer;
+  });
   return (
     <>
-    { state &&
-    <Footer>
-        <Link to='/'>따미 소개</Link>|<Link to='/'>이용약관</Link>|<Link to='/'>개인정보처리방침</Link><br/>
-        <Link to='/'>자주 묻는 질문</Link>|<Link to='/'>고객센터</Link>|<Link to='/'>제휴 문의</Link>
-        <Copyright>Copyright © DDAMI Inc. All rights reserved.</Copyright>
-      </Footer>
-    }
+      {state && (
+        <Footer>
+          <Link to="/">따미 소개</Link>|<Link to="/">이용약관</Link>|
+          <Link to="/">개인정보처리방침</Link>
+          <br />
+          <Link to="/">자주 묻는 질문</Link>|<Link to="/">고객센터</Link>|
+          <Link to="/">제휴 문의</Link>
+          <Copyright>Copyright © DDAMI Inc. All rights reserved.</Copyright>
+        </Footer>
+      )}
     </>
-  )
-}
+  );
+};
