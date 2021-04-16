0908786c3b0984c7bb96826b4f2a55cf30e22e0b
sbyeol3 <ahnsbyeol3@gmail.com>
2021-04-16 18:14:13 +0900

    Refactor : sperate profile from workplace dir #6

--- src/components/workplace/Workplace.js
+++ src/components/workplace/Workplace.js
@@ -1,31 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import styled from 'styled-components';
-import WorkPlaceProfile from "./WorkplaceProfile";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import WorkPlaceProfile from "../workplaceProfile";
 import WorkplaceList from "./WorkplaceList";
-import {useDispatch, useSelector} from "react-redux";
-import {postWorkplace, postWorkplaceMy} from "../../store/actions";
+import { postWorkplace, postWorkplaceMy } from "../../store/actions";
 
 const WorkplaceBody = styled.section`
-  background-color: #F0F0F6;
-`
+  background-color: #f0f0f6;
+`;
 
 const WorkPlace = (props) => {
-  const { type, workplace: {user} } = useSelector((store) => { return store.workplace })
-  const myPieces = user ? user.myPieces : []
-  const dispatch = useDispatch()
-  const { match: { params } } = props
-  const { ArtistId } = params
+  const {
+    type,
+    workplace: { user },
+  } = useSelector((store) => {
+    return store.workplace;
+  });
+  const myPieces = user ? user.myPieces : [];
+  const dispatch = useDispatch();
+  const {
+    match: { params },
+  } = props;
+  const { ArtistId } = params;
   useEffect(() => {
-    if (ArtistId === 'my') dispatch(postWorkplaceMy({}))
-    else dispatch(postWorkplace({id: ArtistId}))
-  },[])
+    if (ArtistId === "my") dispatch(postWorkplaceMy({}));
+    else dispatch(postWorkplace({ id: ArtistId }));
+  }, [ArtistId, dispatch]);
 
-  return(
+  return (
     <WorkplaceBody>
-      <WorkPlaceProfile {...props} type={type} {...user}/>
+      <WorkPlaceProfile {...props} type={type} {...user} />
       <WorkplaceList {...props} myPieces={myPieces} />
     </WorkplaceBody>
-  )
-}
+  );
+};
 
-export default WorkPlace
+export default WorkPlace;
--- src/components/workplaceProfile/WorkplaceProfile.js
+++ src/components/workplaceProfile/WorkplaceProfile.js
@@ -1,97 +1,108 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import { FollowButton as FB } from "../search/ResultArtist";
 
 const ProfileBody = styled.div`
   display: flex;
   padding: 20px 0px 0px 20px;
   font-size: 13px;
-  color: #3C3C3C;
+  color: #3c3c3c;
   border-radius: 0px 0px 0px 16px;
-  background-color: #FFFFFF;
-`
+  background-color: #ffffff;
+`;
 const ProfileImage = styled.div`
   & > img {
-  width: 92px;
-  height: 92px;
-  background-color: #F0F0F0;
-  border-radius: 100%;
-  box-shadow: 0px 3px 5px #F0F0F0;
+    width: 92px;
+    height: 92px;
+    background-color: #f0f0f0;
+    border-radius: 100%;
+    box-shadow: 0px 3px 5px #f0f0f0;
   }
-  
-`
+`;
 
 const Profile = styled.div`
   display: flex;
   flex-direction: column;
   padding-left: 20px;
   padding-top: 5px;
-`
+`;
 
 const ProfileInformation = styled.div`
   width: calc(100vw - 140px);
-  border-bottom: 1px solid #F0F0F0;
+  border-bottom: 1px solid #f0f0f0;
   padding-bottom: 10px;
-`
+`;
 
 const Name = styled.span`
   font-size: 15px;
   font-weight: 800;
-`
+`;
 
 const Id = styled.span`
   margin-left: 10px;
   font-weight: 300;
-  color: #BBBBBB;
-`
+  color: #bbbbbb;
+`;
 
 const Major = styled.div`
   color: #808080;
   margin: 5px 0px;
-`
+`;
 
 const Field = styled.div`
   font-size: 12px;
-`
+`;
 
 const ProfileFollow = styled.div`
   margin: 15px 0px;
   position: relative;
-`
+`;
 
 const FollowButton = styled(FB)`
   top: -8px;
   right: 7px;
-`
+`;
 
 const ModifyButton = styled(FB)`
   top: -85px;
   right: 7px;
-`
+`;
 
 const WorkPlaceProfile = (props) => {
-  const { userName, userId, major, likeField, imageUrl, follow, followerCount, type} = props
-  return(
+  const {
+    userName,
+    userId,
+    major,
+    likeField,
+    imageUrl,
+    follow,
+    followerCount,
+    type,
+  } = props;
+  return (
     <ProfileBody>
-      <ProfileImage><img src={imageUrl}/></ProfileImage>
+      <ProfileImage>
+        <img src={imageUrl} />
+      </ProfileImage>
       <Profile>
         <ProfileInformation>
           <Name>{userName}</Name>
           <Id>@{userId}</Id>
           <Major>{major}</Major>
-          <Field>{likeField && likeField[0]} ・ {likeField && likeField[1]}</Field>
+          <Field>
+            {likeField && likeField[0]} ・ {likeField && likeField[1]}
+          </Field>
         </ProfileInformation>
 
         <ProfileFollow>
-          팔로워 {followerCount} ・
-          팔로우 20
+          팔로워 {followerCount} ・ 팔로우 20
           {!type && !follow && <FollowButton>+Follow</FollowButton>}
           {!type && follow && <FollowButton>+Follow</FollowButton>}
           {type && <ModifyButton>+Modify</ModifyButton>}
         </ProfileFollow>
       </Profile>
     </ProfileBody>
-  )
-}
+  );
+};
 
-export default WorkPlaceProfile
+export default WorkPlaceProfile;
